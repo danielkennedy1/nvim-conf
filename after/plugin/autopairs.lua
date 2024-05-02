bf9cd4c163e6c031b112334556b2c34a846d45d2
Daniel Kennedy <danielrkennedy03@gmail.com>
2024-05-02 01:27:30 +0100

    Autopairs

--- after/plugin/autopairs.lua
+++ after/plugin/autopairs.lua
@@ -1,12 +1,12 @@
-require("nvim-autopairs").setup {
-    check_ts = true,
-}
-
--- cmp setup for autopairs
-
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp = require('cmp')
-cmp.event:on(
-  'confirm_done',
-  cmp_autopairs.on_confirm_done()
-)
+-- require("nvim-autopairs").setup {
+--     check_ts = true,
+-- }
+-- 
+-- -- cmp setup fo autopairs
+-- 
+-- local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+-- local cmp = require('cmp')
+-- cmp.event:on(
+--   'confirm_done',
+--   cmp_autopairs.on_confirm_done()
+-- )
--- lua/dan/packer.lua
+++ lua/dan/packer.lua
@@ -74,5 +74,19 @@ return require('packer').startup(function(use)
     use {
         "windwp/nvim-autopairs",
         event = "InsertEnter",
+        config = function()
+            require("nvim-autopairs").setup {
+                check_ts = true,
+            }
+
+            -- cmp setup fo autopairs
+
+            local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+            local cmp = require('cmp')
+            cmp.event:on(
+                'confirm_done',
+                cmp_autopairs.on_confirm_done()
+            )
+        end
     }
 end)
