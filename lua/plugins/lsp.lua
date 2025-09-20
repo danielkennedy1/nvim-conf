8a35627996aa1f65d65e58bd43d671a4f47f1858
Daniel Kennedy <danielrkennedy03@gmail.com>
2025-09-20 21:48:40 +0100

    fix(linter): linter pathing + some lsp stuff

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -25,7 +25,6 @@ return {
             { 'williamboman/mason-lspconfig.nvim', lazy = true },
         },
         config = function()
-
             local lsp = require('lsp-zero')
             lsp.setup()
 
@@ -51,33 +50,34 @@ return {
                     end,
                 },
             })
+            local default_on_attach = function(client, bufnr)
+                print("LSP attached: " .. client.name .. " to buffer: " .. bufnr)
+                local opts = { noremap = true, silent = true, buffer = bufnr }
+                -- Gotos
+                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+                vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+                -- Actions "l" for LSP + ..
+                vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end, opts)
+                vim.keymap.set("n", "<leader>lh", function() vim.lsp.buf.hover() end, opts)
+                vim.keymap.set("n", "<leader>lws", function() vim.lsp.buf.workspace_symbol() end, opts)
+                vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.open_float() end, opts)
+                vim.keymap.set("n", "<leader>lca", function() vim.lsp.buf.code_action() end, opts)
+                vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format() end, opts)
+                -- Diagnostics
+                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+                -- Signature help in insert mode
+                vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+            end
+
 
             require("mason").setup()
             require("mason-lspconfig").setup({
                 handlers = {
                     function(server_name)
                         require('lspconfig')[server_name].setup({
-                            on_attach = function(client, bufnr)
-                                print("LSP attached: " .. client.name .. " to buffer: " .. bufnr)
-                                local opts = { noremap = true, silent = true, buffer = bufnr }
-                                -- Gotos
-                                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-                                vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
-                                vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-                                -- Actions "l" for LSP + ..
-                                vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end, opts)
-                                vim.keymap.set("n", "<leader>lh", function() vim.lsp.buf.hover() end, opts)
-                                vim.keymap.set("n", "<leader>lws", function() vim.lsp.buf.workspace_symbol() end, opts)
-                                vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.open_float() end, opts)
-                                vim.keymap.set("n", "<leader>lca", function() vim.lsp.buf.code_action() end, opts)
-                                vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format() end, opts)
-                                -- Diagnostics
-                                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-                                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-                                -- Signature help in insert mode
-                                vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-                            end
-                        })
+                            on_attach = default_on_attach })
                     end,
                 },
             })
